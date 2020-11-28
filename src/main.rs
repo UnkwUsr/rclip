ab2b29f93c0a10d6d0912cc6caf1b6a79665950a
UnkwUsr <ktoto2707043@gmail.com>
2020-11-28 21:39:23 +0300

    Add arguments processing (by crate 'clap')

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::{App, Arg, SubCommand};
+
 pub mod clipboard;
 pub mod history;
 
@@ -7,7 +9,40 @@ use clipboard::ClipboardCtx;
 use daemon::Daemon;
 
 fn main() {
-    let clipboard_ctx = ClipboardCtx::new();
-    let mut daemon = Daemon::new(&clipboard_ctx);
-    daemon.start_loop();
+    let arg_matches = App::new("rclip")
+        .version("0.1.0")
+        .author("UnkwUsr <ktoto2707043 at gmail dot cum>")
+        .about("Clipboard manager written in Rust")
+        .subcommand(SubCommand::with_name("daemon").about("Run daemon of clipboard manager"))
+        .subcommand(SubCommand::with_name("list").about("Print list of all entries in history"))
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set picked entry as current clipboard")
+                .arg(
+                    Arg::with_name("entry index")
+                        .short("i")
+                        .long("index")
+                        .help("Index of wanted entry to set. Find it in `list`")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+
+    match arg_matches.subcommand() {
+        ("daemon", Some(_)) => {
+            let clipboard_ctx = ClipboardCtx::new();
+            let mut daemon = Daemon::new(&clipboard_ctx);
+            daemon.start_loop();
+        }
+        ("list", Some(_)) => {
+            unimplemented!("list")
+        }
+        ("set", Some(_)) => {
+            unimplemented!("set")
+        }
+        _ => {
+            unimplemented!("'help' must be here")
+        }
+    }
 }
