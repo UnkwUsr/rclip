8592ef0391627ef5ede72c1f3541a9fdc3efa81b
UnkwUsr <ktoto2707043@gmail.com>
2021-05-24 16:44:40 +0300

    [scripts/copy] Reorder history after pick item

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -8,7 +8,11 @@ source $script_root/fzf_pick.sh
 # send a signal to rclip that now we will set entry from history
 pkill -SIGUSR1 ^rclip$
 
+# get current timestamp in milliseconds
+NEW_FILE_NAME=$TARGET_NAME/$(date +%s%3N)
+mv $FILE_NAME $NEW_FILE_NAME
+
 # nohup need to leave process running in the background (useful when call
 # script by hotkey)
-nohup xclip -t $TARGET_NAME -i $FILE_NAME -sel c > /dev/null 2> /dev/null
+nohup xclip -t $TARGET_NAME -i $NEW_FILE_NAME -sel c > /dev/null 2> /dev/null
 
